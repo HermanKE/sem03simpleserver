f8d2275334f46edcbf4bac04edb81f2ce887da0c
Herman Kjosterud Erlingsen <herman.kjosterd.erlingsen@gmail.com>
2023-04-21 15:30:59 +0000

    Ferdig Bob

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/HermanKE/sem03simpleserver
 go 1.18
 
 require github.com/HermanKE/is105sem03 v0.0.0-20230419115512-a42a85c3b9c3
+
+require (
+	github.com/HermanKE/funtemps v0.0.0-20230324100830-688985659c92 // indirect
+	github.com/HermanKE/minyr v0.0.0-20230414084348-d155fadcb4e0 // indirect
+)
--- tcp_server.go
+++ tcp_server.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"sync"
 	"github.com/HermanKE/is105sem03/mycrypt"
+	"github.com/HermanKE/minyr/yr"
 )
 
 func main() {
@@ -41,7 +42,18 @@ func main() {
 					log.Println("Dekryptert melding: ", string(dekryptertMelding))
 					switch msg := string(dekryptertMelding); msg {
   				        case "ping":
-						_, err = c.Write([]byte("pong"))
+						//_, err = c.Write([]byte("pong"))
+						kryptertMelding := mycrypt.Krypter([]rune(string("pong")), mycrypt.ALF_SEM03, 4)
+						log.Println("Kryptert melding: ", string(kryptertMelding))
+						_, err = conn.Write([]byte(string(kryptertMelding)))
+					case "Kjevik;SN39040;18.03.2022 01:50;6":
+						newString, err := yr.CelsiusToFahrenheitLine("Kjevik;SN39040;18.03.2022 01:50;6")
+						if err != nil {
+							log.Fatal(err)
+						}
+						kryptertMelding := mycrypt.Krypter([]rune(string(newString)), mycrypt.ALF_SEM03, 4)
+						_, err = conn.Write([]byte(string(kryptertMelding)))
+
 					default:
 						_, err = c.Write(buf[:n])
 					}
